04432a30a4dcec8e6c8200638de4ba7662b44806
armadanpagala88-code <armadan.pagala88@gmail.com>
2026-07-24 03:25:32 +0800

    Fix blank image in Bukti TF modal by making the image route public

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::get('/banners', [App\Http\Controllers\Api\BannerController::class, 'index
 // Public Akun Games
 Route::get('/akun-games', [App\Http\Controllers\Api\PublicAkunGameController::class, 'index']);
 Route::get('/akun-games/{id}', [App\Http\Controllers\Api\PublicAkunGameController::class, 'show']);
+Route::get('/admin/transaksi/bukti/{filename}', [App\Http\Controllers\Api\Admin\TransaksiController::class, 'showBukti']);
 
 // Public Ulasan & Rekomendasi
 Route::get('/public/ulasan/kategori/{kategoriId}', [App\Http\Controllers\Api\PublicUlasanController::class, 'getByKategori']);
@@ -104,7 +105,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
         // Transaksi
         Route::get('/admin/transaksi', [App\Http\Controllers\Api\Admin\TransaksiController::class, 'index']);
-        Route::get('/admin/transaksi/bukti/{filename}', [App\Http\Controllers\Api\Admin\TransaksiController::class, 'showBukti']);
         Route::put('/admin/transaksi/{id}/status', [App\Http\Controllers\Api\Admin\TransaksiController::class, 'updateStatus']);
         
         // Kategori Game
